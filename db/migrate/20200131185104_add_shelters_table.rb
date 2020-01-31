45c85aed592ac9b1811724e0cb14dc0dbbe05ef3
Student <example@example.com>
2020-01-31 12:27:53 -0800

    additional weed work

--- db/migrate/20200131185104_add_shelters_table.rb
+++ db/migrate/20200131185104_add_shelters_table.rb
@@ -1,7 +1,7 @@
 class AddSheltersTable < ActiveRecord::Migration[5.2]
   def change
     create_table :shelters do |t|
-      t.column :location, :string
+      t.column :street_address, :string
 
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,18 @@
-class Seed
+# Review.destroy_all
+#
+# 50.times do |index|
+#   Review.create!(name: Faker::Name.first_name,
+#                         content: Faker::Lorem.sentence(word_count: 3),
+#                         rating: Faker::Number.within(range: 1..5))
+# end
+#
+# p "Created #{Review.count} reviews"
 
-  def self.begin
-    seed = Seed.new
-    seed.generate_address
-  end
+Address.destroy_all
 
-  def generate_address
-    20.times do |i|
-      address = Address.create!(
-        location: Faker::Address.street_address
-      )
-    end
-  end
+5.times do |index|
+  Address.create!(street_address: Faker::Address.street_address)
+
 end
 
-Seed.begin
+p "Created #{Place.count} places"
