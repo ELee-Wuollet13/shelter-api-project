8a06ef08da2e3f270cf68ce57ec58de8379951ea
Student <example@example.com>
2020-01-31 17:53:43 -0800

    seeder still broken. Submitting before I punch computer

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+
+## Having tried both versions of the Seeder repeatedly I am entirely lost. the exact same program works when the variable is quotes, but fails cosistantly when it's anything else.
 Shelter.destroy_all
 
 20.times do |index|
