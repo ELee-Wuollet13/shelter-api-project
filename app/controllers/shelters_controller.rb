3c6b95857ba08d3e38be693d3b78b7c786d0ac0c
Student <example@example.com>
2020-01-31 18:46:25 -0800

    seed successfully

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -1,7 +1,7 @@
 class SheltersController < ApplicationController
 
   def index
-    @shelters = {}
+    @shelters = Shelter.all
     json_response(@shelters)
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,19 @@
 
 ## Having tried both versions of the Seeder repeatedly I am entirely lost. the exact same program works when the variable is quotes, but fails cosistantly when it's anything else.
-Shelter.destroy_all
+class Seed
 
-20.times do |index|
-  Shelter.create!(street_address: Faker::Address.street_address)
+  def self.begin
+    seed = Seed.new
+    seed.generate_shelters
+  end
 
+  def generate_shelters
+    20.times do |i|
+      shelter = Shelter.create!(
+        street_address: Faker::Address.street_address
+      )
+    end
+  end
 end
 
-p "Created #{Shelter.count} places"
+Seed.begin
